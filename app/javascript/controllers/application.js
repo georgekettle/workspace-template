0af4745b99ed744f38d927d741a986b7d569ad8d
george kettle <georgekettle.gk@gmail.com>
2023-07-03 13:07:56 +0200

    Installed stimulus components dropdown controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,8 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+// Import external controllers
+import Dropdown from 'stimulus-dropdown'
+application.register('dropdown', Dropdown)
+
 export { application }
